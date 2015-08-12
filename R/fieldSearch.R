66e7e12c12220ac85aceb2f26043b42ed45a2126
Dirk Eddelbuettel <edd@debian.org>
2015-08-12 06:56:40 -0500

    copyright headers

--- R/fieldSearch.R
+++ R/fieldSearch.R
@@ -1,4 +1,23 @@
 
+##
+##  Copyright (C) 2015  Whit Armstrong and Dirk Eddelbuettel and John Laing
+##
+##  This file is part of Rblpapi
+##
+##  Rblpapi is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 2 of the License, or
+##  (at your option) any later version.
+##
+##  Rblpapi is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with Rblpapi.  If not, see <http://www.gnu.org/licenses/>.
+
+
 ##' This function searches for matching Bloomberg data fields given a
 ##' search term.
 ##'
