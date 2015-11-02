f6c450df6690443f87d1e9d944dfc82ea9953832
whit <armstrong.whit@gmail.com>
2015-11-02 14:34:10 -0500

    add subscribe

---
+++ src/subscribe.cpp
@@ -0,0 +1,106 @@
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+//
+//  subscribe.cpp -- "Bloomberg Data Point" query function for the BLP API
+//
+//  Copyright (C) 2015  Whit Armstrong
+//
+//  This file is part of Rblpapi
+//
+//  Rblpapi is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  Rblpapi is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Rblpapi.  If not, see <http://www.gnu.org/licenses/>.
+
+
+// compare to SimpleSubscriptionExample.cpp
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <blpapi_defs.h>
+#include <blpapi_correlationid.h>
+#include <blpapi_element.h>
+#include <blpapi_event.h>
+#include <blpapi_exception.h>
+#include <blpapi_message.h>
+#include <blpapi_session.h>
+#include <blpapi_subscriptionlist.h>
+#include <Rcpp.h>
+#include <blpapi_utils.h>
+
+using BloombergLP::blpapi::Session;
+using BloombergLP::blpapi::Service;
+using BloombergLP::blpapi::SubscriptionList;
+using BloombergLP::blpapi::CorrelationId;
+using BloombergLP::blpapi::Identity;
+using BloombergLP::blpapi::Event;
+using BloombergLP::blpapi::Element;
+using BloombergLP::blpapi::Message;
+using BloombergLP::blpapi::MessageIterator;
+
+// [[Rcpp::export]]
+SEXP subscribe_Impl(SEXP con_, std::vector<std::string> securities, std::vector<std::string> fields,
+                    SEXP options_, SEXP identity_) {
+
+    // via Rcpp Attributes we get a try/catch block with error propagation to R "for free"
+    Session* session = 
+        reinterpret_cast<Session*>(checkExternalPointer(con_, "blpapi::Session*"));
+
+    const std::string mdsrv = "//blp/mktdata";
+    if (!session->openService(mdsrv.c_str())) {
+        Rcpp::stop("Failed to open " + mdsrv);
+    }
+
+    SubscriptionList subscriptions;
+
+    // put fields into comma delimited format
+    std::string fields_collapsed(vectorToCSVString(fields));
+    std::string options_collapsed("");
+    if(options_ != R_NilValue && Rf_length(options_)) {
+        std::vector<std::string> options(Rcpp::as< std::vector<std::string> >(options_));
+        options_collapsed = vectorToCSVString(options);
+    }
+
+    for(const std::string& security : securities) {
+        CorrelationId cid(reinterpret_cast<void*>(const_cast<char *>(security.c_str())));
+        subscriptions.add(security.c_str(),fields_collapsed.c_str(),options_collapsed.c_str(),cid);
+    }
+
+    // check if identity was passed, if so, use it
+    if(identity_ != R_NilValue) {
+        Identity* ip;
+        ip = reinterpret_cast<Identity*>(checkExternalPointer(identity_,"blpapi::Identity*"));
+        session->subscribe(subscriptions,*ip);
+    } else {
+        session->subscribe(subscriptions);
+    }
+
+    int d_eventCount(0), d_maxEvents(10);
+
+    while (true) {
+        Event event = session->nextEvent();
+        MessageIterator msgIter(event);
+        while (msgIter.next()) {
+            Message msg = msgIter.message();
+            if (event.eventType() == Event::SUBSCRIPTION_STATUS ||
+                event.eventType() == Event::SUBSCRIPTION_DATA) {
+                const char *topic = (char *)msg.correlationId().asPointer();
+                std::cout << topic << " - ";
+            }
+            msg.print(std::cout) << std::endl;
+        }
+        if (event.eventType() == Event::SUBSCRIPTION_DATA) {
+            if (++d_eventCount >= d_maxEvents) break;
+        }
+    }
+    return R_NilValue;
+}
