767c23e45a8fa902b4775031e6f9c1f9299abaab
joel23888 <joel23888@users.noreply.github.com>
2016-03-16 15:16:45 +0900

    Add getTicks2 to support condition codes

--- R/getTicks.R
+++ R/getTicks.R
@@ -77,6 +77,70 @@ getTicks <- function(security,
 
 }
 
+##' This function uses the Bloomberg API to retrieve ticks for the requested security.
+##'
+##' @title Get Ticks from Bloomberg, optionally returning condition codes
+##' @param security A character variable describing a valid security ticker
+##' @param eventType A character variable describing an event, default
+##' is \sQuote{TRADE}.
+##' @param startTime A Datetime object with the start time, defaults
+##' to one hour before current time
+##' @param endTime A Datetime object with the end time, defaults
+##' to current time
+##' @param verbose A boolean indicating whether verbose operation is
+##' desired, defaults to \sQuote{FALSE}
+##' @param returnAs A character variable describing the type of return
+##' object; currently supported are \sQuote{matrix} (also the default),
+##' \sQuote{fts}, \sQuote{xts} and \sQuote{zoo}
+##' @param setCondCodes A boolean indicating whether to return any ticks with condition codes,
+##' associated with extraordinary trading and quoting circumstances,
+##' defaults to \sQuote{TRUE}.
+##' @param tz A character variable with the desired local timezone,
+##' defaulting to the value \sQuote{TZ} environment variable, and
+##' \sQuote{UTC} if unset
+##' @param con A connection object as created by a \code{blpConnect}
+##' call, and retrieved via the internal function
+##' \code{defaultConnection}.
+##' @return A numeric matrix with elements \sQuote{time}, (as a
+##' \sQuote{POSIXct} object), \sQuote{values} and \sQuote{sizes}, or
+##' an object of the type selected in \code{returnAs}.
+##' @author Dirk Eddelbuettel
+##' @examples
+##' \dontrun{
+##'   getTicks2("ES1 Index") # Default is to request condition codes
+##' }
+getTicks2 <- function(security,
+                     eventType = "TRADE",
+                     startTime = Sys.time()-60*60,
+                     endTime = Sys.time(),
+                     verbose = FALSE,
+                     returnAs = getOption("blpType", "matrix"),
+                     setCondCodes = TRUE,
+                     tz = Sys.getenv("TZ", unset="UTC"),
+                     con = defaultConnection()) {
+
+    if (!inherits(startTime, "POSIXt") || !inherits(endTime, "POSIXt")) {
+        stop("startTime and endTime must be Datetime objects", call.=FALSE)
+    }
+    fmt <- "%Y-%m-%dT%H:%M:%S"
+    startUTC <- format(startTime, fmt, tz="UTC")
+    endUTC <- format(endTime, fmt, tz="UTC")
+    res <- getTicks2_Impl(con, security, eventType, startUTC, endUTC, setCondCodes, verbose)
+
+    attr(res[,1], "tzone") <- tz
+
+    ## return data, but omit event type which is character type
+    res <- switch(returnAs,
+                  matrix = res[,-2],           # default is matrix
+                  fts    = fts::fts(res[,1], res[,-(1:2)]),
+                  xts    = xts::xts(res[,-(1:2)], order.by=res[,1]),
+                  zoo    = zoo::zoo(res[,-(1:2)], order.by=res[,1]),
+                  res)                         # fallback is also matrix
+    return(res)   # to return visibly
+
+}
+
+
 ##' This function uses the Bloomberg API to retrieve multiple ticks
 ##' for the requested security.
 ##'
