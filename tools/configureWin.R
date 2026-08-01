2ca22a3fdeb0934bb791b9bd2947618e6d2b3970
Dirk Eddelbuettel <edd@debian.org>
2026-08-01 18:58:05 -0500

    A little more debugging (2)

--- tools/configureWin.R
+++ tools/configureWin.R
@@ -2,8 +2,11 @@
 ## See e.g. github package 'eddelbuettel/tellme' at r-universe for output on
 ## various platform; there we aim at windows-arm64 for which we have Blp library
 cat("+++ hello from configureWin.R\n")
+print(R.version)
 if (R.version$platform == "aarch64" && R.version$os == "mingw32") {
-    file.copy("src/Makevars.no_nlp", "src/Makevars.win")
+    cat("+++ hello from configureWin.R -- aarch/arm64\n")
+    rc <- file.copy("src/Makevars.no_nlp", "src/Makevars.win")
 } else {
-    file.copy("src/Makevars.win.in", "src/Makevars.win")
+    cat("+++ hello from configureWin.R -- amd64\n")
+    rc <- file.copy("src/Makevars.win.in", "src/Makevars.win")
 }
