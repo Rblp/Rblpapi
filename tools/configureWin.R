e841474ca24dc720c4fa1443eadaee5e4fe5a61d
Dirk Eddelbuettel <edd@debian.org>
2026-08-01 20:28:40 -0500

    Correct comparison variable

--- tools/configureWin.R
+++ tools/configureWin.R
@@ -3,7 +3,7 @@
 ## various platform; there we aim at windows-arm64 for which we have Blp library
 cat("+++ hello from configureWin.R\n")
 print(R.version)
-if (R.version$platform == "aarch64" && R.version$os == "mingw32") {
+if (R.version$arch == "aarch64" && R.version$os == "mingw32") {
     cat("+++ hello from configureWin.R -- aarch/arm64\n")
     rc <- file.copy("src/Makevars.no_nlp", "src/Makevars.win")
 } else {
